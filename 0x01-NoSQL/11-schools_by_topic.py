2945cc3c5202919acb98a5061ee66196902bed41
Cappu123 <cappyalemayehu12@gmail.com>
2024-08-27 18:16:09 -0700

    log stats

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,9 @@
+"""Python function that returns the list of school having a specific topic"""
+
+
+import pymongo
+
+
+def schools_by_topic(mongo_collection, topic):
+    '''return list'''
+    return mongo_collection.find({"topics": topic})
